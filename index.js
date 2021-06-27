354f398a3e390fc361bd1b2fe1cee4bdf0ab6da3
Lev Shushkov <levashushkov@yandex.ru>
2021-06-28 02:09:02 +0400

    LIKES

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-let popup = document.getElementById('popup');
+// ОТКРЫТИЕ И ЗАКРЫТИЕ POP-UP
+
+let popup = document.querySelector('.popup');
 let editButton = document.getElementById('editButton').addEventListener("click", openPopup);
 let closeButton = document.getElementById('closeButton').addEventListener("click", closePopup);
-let likeButton = document.getElementById('like').addEventListener("click", likeAdd);
 
 function openPopup() {
     popup.style.display = 'flex';
@@ -11,6 +12,17 @@ function closePopup() {
     popup.style.display = 'none';
 };
 
-function likeAdd() {
-    likeButton.addquerrySelector('element__like_active');
-};
+// ЛАЙКИ
+
+document.onclick = function(event) {
+    if (event.target.className == 'element__like'){
+        event.target.classList.add('element__like_active');
+    }
+    else {
+        event.target.classList.remove('element__like_active');
+    }
+}
+
+// ИМЯ ПРОФИЛЯ
+
+let profileContainer = document.querySelector('.profile__text');

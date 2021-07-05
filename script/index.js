5b2919409f5611b5e0be4e3a8ac6729c28d0adb0
Lev Shushkov <levashushkov@yandex.ru>
2021-07-05 20:05:48 +0400

    popup with image + cards in box

--- script/index.js
+++ script/index.js
@@ -10,24 +10,22 @@ const closeButtonAdd = document.querySelector('.popup__close-button_add').addEve
 
 function popupEditFunction() { // создаю функцию открытия/закрытия попап редактирования профиля
 
-    if (popupEdit.className === 'popup_edit') { // задаю условие, что если у попапа в CSS прописан display: flex;
-        popupEdit.classList.toggle('popup_opened'); // то он меняется на display: none;
+    if (popupEdit.className === 'popup_edit') { // задаю условие, что если попап имеет такой класс
+        popupEdit.classList.toggle('popup_opened'); // то он добалвляет себе новый класс
     }
     else {
-        popupEdit.classList.toggle('popup_opened'); // а в обратном случае меняется на display: flex;
+        popupEdit.classList.toggle('popup_opened'); // а в обратном случае удаяет
     }
 };
 
 function popupAddFunction() { // создаю функцию открытия/закрытия попап добавления карточки места
-    if (popupAdd.className === 'popup_edit') { // задаю условие, что если у попапа в CSS прописан display: flex;
-        popupAdd.classList.toggle('popup_opened'); // то он меняется на display: none;
+    if (popupAdd.className === 'popup_edit') { // задаю условие, что если попап имеет такой класс
+        popupAdd.classList.toggle('popup_opened'); // то он добалвляет себе новый класс
     }
     else {
-        popupAdd.classList.toggle('popup_opened'); // а в обратном случае меняется на display: flex;
+        popupAdd.classList.toggle('popup_opened'); // а в обратном случае удаяет
     }
 };
-
-
 
 // ЛАЙКИ
 
@@ -56,12 +54,57 @@ function saveInformation() { // создаю функцию сохранения
     popupEditFunction(); // функция сохранения информации отработала и при этом попап закрылся
 }
 
-//  ДОБАВЛЕНИЕ НОВЫХ КАРТОЧЕК
+
 
 const cardsContainer = document.querySelector('.elements'); // нахожу в документе место, в которое добавляются все карточки
 
+// РЕНДЕР ГОТОВЫХ КАРТОЧЕК
+
+const initialCards = [  // Массив с данными для карточек "из коробки"
+    {
+      name: 'Воркута',
+      link: 'images/vorkuta.jpg'
+    },
+    {
+      name: 'Киров',
+      link: 'images/kirov.jpg'
+    },
+    {
+      name: 'Вологда',
+      link: 'images/vologda.jpg'
+    },
+    {
+      name: 'Москва',
+      link: 'images/moskva.jpg'
+    },
+    {
+      name: 'Батуми',
+      link: 'images/batumi.jpg'
+    },
+    {
+      name: 'Карачаевск',
+      link: 'images/karachaevsk.jpg'
+    }
+  ]; 
+
+initialCards.forEach(item => {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+
+    cardElement.querySelector('.element__title').textContent = item.name;
+    cardElement.querySelector('.element__image').src = item.link;
+    cardElement.querySelector('.element__image').alt = item.name;
+
+    cardElement.querySelector('.element__image').addEventListener('click', popupImageFunction);
+    cardElement.querySelector('.element__image').addEventListener('click', imageRender);
+
+    cardsContainer.append(cardElement)
+});
+
+//  ДОБАВЛЕНИЕ НОВЫХ КАРТОЧЕК
+
 const addButton = document.querySelector('.popup__form-button_add'); // нахожу в документе кнопку "сохранить", которая находится в попапе добавления карточки
-addButton.addEventListener('click', function () { // 
+addButton.addEventListener('click', function () { 
     const title = document.querySelector('.popup__form-field_title');
     const image = document.querySelector('.popup__form-field_image');
 
@@ -79,6 +122,9 @@ function addCard(сardTitle, cardImage) {
     cardElement.querySelector('.element__image').src = cardImage;
     cardElement.querySelector('.element__image').alt = сardTitle;
 
+    cardElement.querySelector('.element__image').addEventListener('click', popupImageFunction);
+    cardElement.querySelector('.element__image').addEventListener('click', imageRender);
+
     addButton.addEventListener('submit', clearing); // добавил слушатель события "отправки формы" на кнопку "сохранить" которая запускает функцию очистки формы от введенных значений
     function clearing() {    // создаю функцию очистки
         document.querySelector('.popup__form_add').reset(); // нахожу в документе нужную форму и очищаю ее
@@ -88,3 +134,26 @@ function addCard(сardTitle, cardImage) {
     clearing(); // поля формы очищаются уже после закрытия попапа и создания карточки
 };
 
+// ОТКРЫТИЕ ПОПАП С ИЗОБРАЖЕНИЕМ
+
+const popupWithImage = document.querySelector('.popup_img');
+const popupImageClose = document.querySelector('.popup__close-button_img').addEventListener('click', popupImageFunction);
+const popupImage = document.querySelector('.popup__image');
+const popupImageDescription = document.querySelector('.popup__image-alt');
+
+function imageRender(event) {
+    let itemImage = event.target.src;
+    popupImage.src = itemImage;
+    let itemAlt = event.target.alt;
+    popupImageDescription.textContent = itemAlt
+
+}
+
+function popupImageFunction() {
+    if (popupWithImage.className === 'popup_edit') {
+        popupWithImage.classList.toggle('popup_opened');
+    }
+    else {
+        popupWithImage.classList.toggle('popup_opened');
+    }
+};
